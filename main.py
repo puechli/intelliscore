ed249a3558074314aa7341cc7be3a12144f0efca
Lilian Puech <lilian.puech@etu.univ-grenoble-alpes.fr>
2024-11-13 17:43:49 +0100

    Added metronome feature BUT we have to adjust a little bit more and remove the repetition.

--- main.py
+++ main.py
@@ -59,6 +59,13 @@ camera_height = 120
 motion_threshold = 5000 if len(argv) < 3 else int(argv[2])
 previous_frame = None
 
+# Metronome settings
+metronome_division:float = 0.25
+metronome_speed:float = 96.0
+shift_multiplier:float = 0.1 if len(argv) < 4 else int(argv[3])
+shift_speed:int = int(metronome_division * metronome_speed * shift_multiplier)
+metronome_activated:bool = False
+
 # Game loop
 clock = py.time.Clock()
 running = True
@@ -66,18 +73,25 @@ while running:
     for event in py.event.get():
         if event.type == py.QUIT:
             running = False
-    
+        elif event.type == py.KEYDOWN:
+            if event.key == py.K_t:
+                metronome_activated = not(metronome_activated)
+
     keys = py.key.get_pressed()
 
-    if   keys[py.K_UP]:    cur_zoom = min(max_zoom, cur_zoom * stp_zoom)
-    elif keys[py.K_DOWN]:  cur_zoom = max(min_zoom, cur_zoom / stp_zoom)
-    elif keys[py.K_LEFT]:  x = min(max_x, x + stp_x)
-    elif keys[py.K_RIGHT]: x = max(min_x, x - stp_x)
+    if   keys[py.K_UP]:    cur_zoom = min(max_zoom, cur_zoom * stp_zoom) if not(metronome_activated) else cur_zoom
+    elif keys[py.K_DOWN]:  cur_zoom = max(min_zoom, cur_zoom / stp_zoom) if not(metronome_activated) else cur_zoom
+    elif keys[py.K_LEFT]:  x = min(max_x, x + stp_x * cur_zoom) if not(metronome_activated) else x
+    elif keys[py.K_RIGHT]: x = max(min_x, x - stp_x * cur_zoom) if not(metronome_activated) else x
+
+    # Shift about metronome
+    if metronome_activated:
+        x = x = max(min_x, x - shift_speed * cur_zoom)
 
     # Check audio volume
     data = np.frombuffer(stream.read(CHUNK), dtype=np.int16)
     volume = np.abs(data).mean()
-    if volume > THRESHOLD: x = max(min_x, x - stp_x)
+    if volume > THRESHOLD: x = max(min_x, x - stp_x * cur_zoom) if not(metronome_activated) else x
 
     # Fill the background with white
     screen.fill(background_color)
@@ -112,7 +126,7 @@ while running:
             contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             
             # Move the main image right if motion is detected
-            if contours: x = max(min_x, x - stp_x)
+            if contours: x = max(min_x, x - stp_x * cur_zoom)
 
         previous_frame = frame
 
